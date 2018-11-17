81b70b1f3c356a695d68abf3785c92782689b4a4
Seva <seva.voloshin@gmail.com>
2018-11-17 12:53:44 +0200

    code refactoring and minor improvements

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,94 +1,81 @@
-# * Here you must define your `Factory` class.
-# * Each instance of Factory could be stored into variable. The name of this variable is the name of created Class
-# * Arguments of creatable Factory instance are fields/attributes of created class
-# * The ability to add some methods to this class must be provided while creating a Factory
-# * We must have an ability to get/set the value of attribute like [0], ['attribute_name'], [:attribute_name]
-#
-# * Instance of creatable Factory class should correctly respond to main methods of Struct
-# - each
-# - each_pair
-# - dig
-# - size/length
-# - members
-# - select
-# - to_a
-# - values_at
-# - ==, eql?
-
 class Factory
-  def self.new(*args, &block)
-    const_set(args.shift.capitalize, class_new(*args, &block)) if args.first.is_a?(String)
-    class_new(*args, &block)
-  end
+  class << self
+    def new(*args, &block)
+      const_set(args.shift.capitalize, class_new(*args, &block)) if args.first.is_a?(String)
+      class_new(*args, &block)
+    end
 
-  def self.class_new(*args, &block)
-    Class.new do
-      attr_accessor(*args)
+    def class_new(*args, &block)
+      Class.new do
+        attr_accessor(*args)
 
-      class_eval(&block) if block_given?
+        def initialize(*vars)
+          raise ArgumentError, "Expected #{args.count} argument(s)" if args.count != vars.count
 
-      def initialize(*vars)
-        raise ArgumentError, "Expected #{args.count} arguments" if args.count != vars.count
-
-        args.each_index do |index|
-          instance_variable_set("@#{args[index]}", vars[index])
+          args.each_index do |index|
+            instance_variable_set("@#{args[index]}", vars[index])
+          end
         end
-      end
 
-      define_method :args do
-        args
-      end
+        define_method :args do
+          args
+        end
 
-      def [](arg)
-        arg.is_a?(Integer) ? values[arg] : instance_variable_get("@#{arg}")
-      end
+        def [](arg)
+          arg.is_a?(Integer) ? values[arg] : instance_variable_get("@#{arg}")
+        end
 
-      def []=(arg, value)
-        instance_variable_set("@#{arg}", value)
-      end
+        def []=(arg, value)
+          instance_variable_set("@#{arg}", value)
+        end
 
-      def each(&block)
-        values.each(&block)
-      end
+        def each(&block)
+          values.each(&block)
+        end
 
-      def each_pair(&pair)
-        hash.each_pair(&pair)
-      end
+        def each_pair(&pair)
+          to_h.each_pair(&pair)
+        end
 
-      def dig(*args)
-        hash.dig(*args)
-      end
+        def dig(*args)
+          first_arg = self[args.first]
+          return nil if first_arg == nil
+          args.one? ? first_arg : first_arg.dig(*args[1..-1])
+        end
 
-      def hash
-        Hash[instance_variables.map do |variable|
-          [variable.to_s.delete('@').to_sym, instance_variable_get(variable)]
-        end]
-      end
+        def to_h
+          Hash[instance_variables.map do |variable|
+            [variable.to_s.delete('@').to_sym, instance_variable_get(variable)]
+          end]
+        end
 
-      def length
-        args.size
-      end
+        def length
+          args.size
+        end
 
-      def select(&block)
-        values.select(&block)
-      end
+        def select(&block)
+          values.select(&block)
+        end
 
-      def values
-        instance_variables.map { |arg| instance_variable_get(arg) }
-      end
+        def values
+          instance_variables.map { |arg| instance_variable_get(arg) }
+        end
 
-      def values_at(*indexes)
-        indexes.map { |index| values[index] }
-      end
+        def values_at(*indexes)
+          indexes.map { |index| values[index] }
+        end
 
-      def ==(obj)
-        self.class == obj.class && self.values == obj.values
-      end
+        def ==(other)
+          self.class == other.class && self.values == other.values
+        end
 
-      alias_method :to_a, :values
-      alias_method :eql?, :==
-      alias_method :size, :length
-      alias_method :members, :args
+        class_eval(&block) if block_given?
+
+        alias_method :to_a, :values
+        alias_method :eql?, :==
+        alias_method :size, :length
+        alias_method :members, :args
+      end
     end
   end
 end
